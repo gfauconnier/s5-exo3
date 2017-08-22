80e63944948be475205f0d29f389d74d63d490e6
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-08-22 13:48:51 +0200

    added comments

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
+// object constructor
 function product(productName, productCatch, productDesc) {
   this.productName = productName;
   this.productCatch = productCatch;
   this.productDesc = productDesc;
 }
 
+// object constructor
 function newProduct() {
   this.validInputs = function() {
     var i = 0;
+    // checks if all inputs are filled
     $("input").each(function() {
       return $(this).val().length ? i : i++;
     });
-    return i;
+    return i; // i = 0 if all inputs are filled
   };
   this.createProduct = function() {
+    // creates a new product object if all inputs are filled
     if (!this.validInputs()) {
       var newProd = new product($("input").eq(0).val(), $("input").eq(1).val(), $("input").eq(2).val());
       this.dispalyProduct(newProd);
@@ -22,6 +26,7 @@ function newProduct() {
     }
   };
   this.dispalyProduct = function(prod) {
+    // displays the product display div and adds the new product infos
     Object.values(prod).forEach(function(elem, index) {
       $(".box").removeClass("d-none");
       $(".box p span").eq(index).text(elem);
@@ -29,6 +34,7 @@ function newProduct() {
   }
 }
 
+// on click or return key press attempts to create a new object
 var newProd = new newProduct();
 $("button").click(function() {
   newProd.createProduct();
